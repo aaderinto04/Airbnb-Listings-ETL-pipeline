445c1d2891cd47e157a3089df8cba686a1c9f5b8
aaderinto04 <abdullahaderinto@gmail.com>
2025-12-14 17:04:48 -0700

    normalized prices

--- spark_app/spark.py
+++ spark_app/spark.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_replace
 
 spark = SparkSession.builder \
     .appName("Airbnb Spark application") \
@@ -14,5 +15,15 @@ listings = spark.read.csv("/Users/abdullahaderinto/Documents/Airbnb-Listings-ETL
     mode="PERMISSIVE" 
 )
 
-listings.printSchema()
+#listings.printSchema()
+
+#Add a column to normalize prices into float type removing $ and ,
+norm_price = listings\
+.withColumn('price_num', regexp_replace('price', '[$,]', '').cast('float'))
+norm_price.schema['price_num']
+norm_price.select('price_num')\
+.show(5, truncate=False)
+
+
+
 
